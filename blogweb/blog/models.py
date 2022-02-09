28c218c6ab4743cf4d4ebdd06e054fc2c361de26
Devang Nigam <devangnigam22222@gmail.com>
2022-02-09 15:51:30 +0530

    model1.0

--- blogweb/blog/models.py
+++ blogweb/blog/models.py
@@ -16,4 +16,15 @@ class Post(models.Model):
     slug= models.SlugField(maxlength=250,unique_for_date='publish')
     author= models.ForeignKey(User_on_delete=models.CASCADE,related_name='blog_posts')
     body = models.models.TextField()
-    publish =models.DateTimeField
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+     updated = models.DateTimeField(auto_now=True)
+     status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+        
+    
