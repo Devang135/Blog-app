f2d253ff00336cdd5fc7a615ff5c2abf55eb41d7
Devang Nigam <devangnigam22222@gmail.com>
2022-02-16 01:12:40 +0530

    model update

--- blogweb/blog/admin.py
+++ blogweb/blog/admin.py
@@ -1,6 +1,11 @@
+from turtle import title
 from django.contrib import admin
 from .models import Post, models
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status','created','publish','author')
+    search_fields = ('title','body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
